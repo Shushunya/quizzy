9a1363488dab0ce9745ec725497cede2efa460a6
Oleksandra <mremarka314@gmail.com>
2023-10-11 01:31:46 +0300

    add login form

--- frontend/src/pages/authorization/login-page.jsx
+++ frontend/src/pages/authorization/login-page.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 import { LoginForm } from "../../components/authorization/login/login-form";
 
 export function LoginPage() {
   return (
-    <>
-      <p>LOGIN</p>
+    <div className="flex flex-column align-items-center p-5">
       <LoginForm />
-    </>
+      <p>No account? <Link to={'/signup'}>Sign Up!</Link></p>
+    </div>
   );
 }
