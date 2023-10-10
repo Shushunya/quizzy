4fef9ebd03c9b6a30a9cf77047cd36acff275d12
Oleksandra <mremarka314@gmail.com>
2023-10-10 22:54:27 +0300

    add landing-page

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,18 +1,15 @@
 import "primeicons/primeicons.css";
 import { Link } from "react-router-dom";
-import { PrimeReactProvider } from "primereact/api";
-
-// import "primereact/resources/themes/soho-light/theme.css";
-import "primereact/resources/themes/soho-dark/theme.css";
 
 export default function LandingPage() {
   return (
     <>
       <div>
         Links
-        <Link to={`/new`}>create new quiz</Link>
+        <Link to={`/new`}>Error page</Link>
+        <Link to={`/quizzes`}>Quiz List</Link>
+        
       </div>
-      <div>FOOTER</div>
     </>
   );
 }
