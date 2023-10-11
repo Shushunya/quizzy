3bdd5be13ec76123375bdfc69e729b90f2e5fe55
Oleksandra <mremarka314@gmail.com>
2023-10-11 03:18:48 +0300

    place footer at the bottom & fix styles

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -5,20 +5,19 @@ export function Footer() {
   // FIXME: adjust footer
 
   return (
-    <div className="flex bg-bluegray-800 align-items-start justify-content-between h-5rem gap-3 p-3">
+    <footer className="flex bg-bluegray-800 align-items-start h-5rem gap-3 p-3">
       <div className="flex gap-2">
         <Link className="flex align-item-center text-4xl no-underline" to={"/"}>
           Quizzy
         </Link>
         <div className="flex align-items-end p-1 gap-1 font-light text-sm">
-          <span>v0.1</span>
           <span>2023</span>
+          <i className="pi pi-fw pi-heart"></i>
+          <Link to={"https://github.com/Shushunya/quizzy"}>
+            developed by Shushunya
+          </Link>
         </div>
       </div>
-      <div>
-        <i className="pi pi-fw pi-heart"></i>
-        <span>developed by Shushunya</span>
-      </div>
-    </div>
+    </footer>
   );
 }
