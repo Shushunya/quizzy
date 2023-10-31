2513d8b78ed30c15068e932da7a637feda4ed54a
Oleksandra <mremarka314@gmail.com>
2023-10-31 17:36:26 +0200

    add props paasing

--- frontend/src/components/quiz/detail/question-card.jsx
+++ frontend/src/components/quiz/detail/question-card.jsx
@@ -1,27 +1,27 @@
-import { RadioButton } from "primereact/radiobutton";
-import { Button } from "primereact/button";
 import { useState } from "react";
 import { Panel } from "primereact/panel";
+import { RadioButton } from "primereact/radiobutton";
 
-export const QuestionCard = ({ question, handleChange }) => {
+export const QuestionCard = ({ question, handleChange, userAnswer }) => {
   const [isShown, setIsShown] = useState(false);
-  const [userAnswer, setUserAnswer] = useState(null);
   return (
     <Panel header={`Question #${question.id}`} toggleable>
-      <div className="flex flex-row-reverse gap-3">
-        <i className="pi pi-question" onClick={() => setIsShown(true)}></i>
-        {isShown ? <span>{question.hint}</span> : false}
+      <div className="flex justify-content-between">
+        {question.text}
+        <div className="flex flex-row-reverse gap-3">
+          <i className="pi pi-question" onClick={() => setIsShown(true)}></i>
+          {isShown ? <span>{question.hint}</span> : false}
+        </div>
       </div>
       <div>Choose your answer:</div>
-
       {question.answers.map((answer) => (
         <div key={answer.id} className="flex align-items-center p-1">
           <RadioButton
             inputId={answer.id}
             name="answer"
             value={answer.id}
-            onChange={(e) => handleChange(e.target.value)}
-            checked={userAnswer === answer.id}
+            onChange={() => handleChange(answer)}
+            checked={answer.id === userAnswer}
           />
           <label htmlFor={answer.id} className="ml-2">
             {answer.text}
